ee1beb567686e1ea174e62795b8beb016a797f1e
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-08-12 17:07:59 -0500

    Add basic command

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nova Static
+Nova Static is an tool to Server Side Include (SSI) views in static sites using [Hypernova](https://github.com/airbnb/hypernova)
+
+## Environment Variables
+
+```
+HYPERNOVA_BATCH=http://hypernova:3000/batch
+STATIC_FOLDER=./public
+```
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/ara-framework/nova-proxy/parser"
+)
+
+func main() {
+	staticFolder := os.Getenv("STATIC_FOLDER")
+	filepath.Walk(staticFolder, func(path string, info os.FileInfo, err error) error {
+		fileName := info.Name()
+		if strings.HasSuffix(fileName, ".html") && !info.IsDir() {
+			e, err := ioutil.ReadFile(path)
+
+			if err != nil {
+				log.Fatal(path + " can not be read \n")
+			}
+
+			newHTML := parser.ModifyBody(string(e))
+
+			ioutil.WriteFile(path, []byte(newHTML), 0644)
+		}
+		return nil
+	})
+}
